7902d71c76a34c485eeade70d53abec25b422c7f
Marcin Borowski <cronos.mb@gmail.com>
2024-04-16 22:44:00 +0200

    breaking change: time for solid

--- app/get-blobs/route.ts
+++
@@ -1,8 +0,0 @@
-import { list } from "@vercel/blob";
-
-export const runtime = "edge";
-
-export async function GET(request: Request) {
-  const { blobs } = await list();
-  return Response.json(blobs);
-}
